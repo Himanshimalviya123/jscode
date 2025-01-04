372f2000b584b9377b75cb618ded4dbfbb7b0a5f
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-04 18:32:40 +0530

    update

---
+++ day1.js
@@ -0,0 +1,26 @@
+// function sid(){
+//     console.log("first")
+
+// }
+// function abc(){ 
+//     console.log("second")
+
+
+// }
+// sid();
+// abc();
+function go(){
+    // type coercion
+    // string has highest priority
+    // number
+    // null=0
+    // NaN=not a no.
+    // var a=5;             //
+    // var b=undefined;     //NaN out put
+    // console.log(a+b);    //// NaN=not a no.
+
+    var a="5";             
+    var b=4;     
+    console.log(a-b); 
+}
+go();
