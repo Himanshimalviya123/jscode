d584353ed4957e6612d2abc5364368260b21bd49
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-06 18:22:59 +0530

    update

--- letvarconst.js
+++ letvarconst.js
@@ -1,19 +1,62 @@
 function go ()
 {
+    // =====================redecleration=========================
 //var let const
 var age= 26;
 var age =28;
 console.log(age);
-// =============================scope===============
 // let a=56;
 // let a=58;
 
 // const a=46;
 // const a=46;
-// =====================reassidnment===================
-var a=4;
-a=78;
+// =====================reassignment===================
+// var a=4;
+// a=78;
+// a=59;
+// console.log(a);
+
+// let a=5;
+// a=7;
+// console.log(a);
+
+// const a=6;
+// a=8;
+// console.log(a);
+
+// =======================scope=========================
+// var a=8;
+// console.log(a);
+
+// {
+//     var a=7;
+//     // console.log(a);
+    
+
+// }
+// console.log(a);
+
+// let a=5;
+// console.log(a);
+
+// {
+//     let a=7;
+// //  console.log(a);
+    
+
+// }
+// console.log(a);
+const a=5;
 console.log(a);
+
+{
+const a=7;
+ console.log(a);
+    
+
+}
+console.log(a);
+
 
 }
 
