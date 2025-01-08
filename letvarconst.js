1821cd07a901e9c1b69791e355b9f2bd9fb03df5
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-08 23:33:39 +0530

    update

--- letvarconst.js
+++ letvarconst.js
@@ -2,9 +2,9 @@ function go ()
 {
     // =====================redecleration=========================
 //var let const
-var age= 26;
-var age =28;
-console.log(age);
+// var age= 26;
+// var age =28;
+// console.log(age);
 // let a=56;
 // let a=58;
 
@@ -46,18 +46,119 @@ console.log(age);
 
 // }
 // console.log(a);
-const a=5;
-console.log(a);
+// const a=5;
+// console.log(a);
 
-{
-const a=7;
- console.log(a);
+// {
+// const a=7;
+//  console.log(a);
     
 
-}
-console.log(a);
+// }
+// console.log(a);
+
+// ========================Hosting===================
+// console.log(a);
+// var a=6;            
+// var b=7;
+// console.log(a);
+// question 1- age============
+// let abc=prompt("enter your age")
+// if(abc>=18)
+// {
+//     alert("you are eligible for vote");
+// }
+// else{
+//     alert("you are not eligible for vote"); 
+// }
 
 
+// question 2- even/odd================
+// let abc=prompt("enter your number")
+// if(abc%=2)
+// {
+//     alert("odd number");
+// }
+// else{
+//     alert("even number"); 
+// }
+// question=3 positive/negative===============
+// let abc=prompt("enter your number")
+// if(abc>=1)
+// {
+//     alert("positive number");
+// }
+// else{
+//     alert("negative number"); 
+// }
+// question=4 pass/fail==========================
+// let abc=prompt("enter your marks")
+// if(abc>=33)
+// {
+//     alert("pass");
+// }
+// else{
+//     alert("fail"); 
+// }
+// question=5 greater than 10===============
+// let abc;
+// do{
+//     abc=parseInt(prompt("enter your number greater than 10"));
+
+// }
+// while(abc<=10)
+// alert("thank you!you entered a number greater than 10:"+10);
+// question-6 conformation================
+// let abc=confirm("ask for conformation")
+//  if(abc==true)
+//      {
+//      alert("item deleted");
+// }
+// else{
+//     alert("cancelled"); 
+//  }
+// question=7=============course completed========================================================
+// let abc=confirm("ask the user have completed your course")
+// let efg=confirm("do you have certificate?")
+//  if(abc && efg)
+//      {
+//       alert("completed");
+//  }
+//  else{
+//      alert("not completed"); 
+//   }
+// question=10
+let a = prompt("Enter a day of the week (1 for Monday, 2 for Tuesday, etc.):");
+a = parseInt(a);
+
+switch(a)
+ {
+    if(a==1)
+        console.log("Monday");
+        break;
+        if(a==2)
+            console.log("tuesday");
+            break;
+            if(a==3)
+                console.log("wednesday");
+                break;
+                if(a==4)
+                    console.log("thursday");
+                    break;
+                    if(a==5)
+                        console.log("friday");
+                        break;
+                        if(a==6)
+                            console.log("saturday");
+                            break;
+                            if(a==7)
+                                console.log("sunday");
+                                break;
+        else
+        {
+            console.log("Invalid input! Please enter a number between 1 and 7.");
+            break;
+        }
 }
 
 go();
