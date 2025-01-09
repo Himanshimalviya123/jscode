3385e2a8428dd98917c697b6272ee4bc9ef20b0e
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-09 18:36:21 +0530

    update

---
+++ string.js
@@ -0,0 +1,17 @@
+ function shivani()
+ {
+         alert("hello")
+
+}// shivani();
+ function ram()
+{
+     alert("fine")
+
+ }
+ // ram();
+function shikha()
+{
+    alert("hyyyyy")
+
+}
+//shikha();
