9bd7738d45816f60dad2890e5908f9e2c146fbbb
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-10 18:21:32 +0530

    update

--- string.js
+++ string.js
@@ -15,3 +15,37 @@
 
 // }
 // //shikha();
+    // =========================================================================================================================
+    // ========================================================string=======================================
+
+function tanisha(){
+//   output  print type
+//     alert("hello tanisha")     broweser window
+//     console.log();        console
+//     document.write("<h1> hello ")
+// console.log(typeof("himani"));
+
+// =========================string method===============
+//  let text="ThisIsString";
+//  document.write(text.slice(5,10))  ===========       to check slice of string
+let text="JavaScriptIsEasy";
+document.write(text.slice(4,13))
+// document.write(text.length)================to check the length of the string
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
