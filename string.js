6d5161163c4a77fcee9eb29ea41200eb215edcd0
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-11 18:20:55 +0530

    modify

--- string.js
+++ string.js
@@ -31,16 +31,21 @@ function tanisha(){
 // let text="JavaScriptIsEasy";
 // document.write(text.slice(4,13))
 // document.write(text.length)================to check the length of the string
- let text1="himanshi;"
+//  let text1="himanshi;"
 //  let text1="malviya";
 // document.write(text1.concat(text2))
 // document.write(text.toLowerCase(text))
 
 // document.write(text.toUpperCase(text))
-//  document.write(text1.replaceAll(sakshi))  
-//  document.write(text1.charAt(5))  
-  document.write(text1.indexOf("a"))
+let text="i am learning Javascript and Javascript is fun javascript"
+//   document.write(text.replace("Javascript","js"))===========replace the word=====================
+// document.write(text.replace(/Javascript/g,"js"))  global=>g
+// document.write(text.replace(/Javascript /gi,"js")) =================== case in sensitive=i=============
+
+//  document.write(text1.charAt(5))  ====================sky value return
+//   document.write(text1.indexOf("a"))==============to check the index no. of the char
 //   document.write(text1.split("a"))
+// document.write(text1.charCodeAt())
 
 }
 
