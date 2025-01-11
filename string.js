7723818fb9f498afb76ffdced27c35e992ddd7ee
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-11 17:03:34 +0530

    update

--- string.js
+++ string.js
@@ -28,10 +28,22 @@ function tanisha(){
 // =========================string method===============
 //  let text="ThisIsString";
 //  document.write(text.slice(5,10))  ===========       to check slice of string
-let text="JavaScriptIsEasy";
-document.write(text.slice(4,13))
+// let text="JavaScriptIsEasy";
+// document.write(text.slice(4,13))
 // document.write(text.length)================to check the length of the string
+//  let text1="himanshi;"
+//  let text1="malviya";
+// document.write(text1.concat(text2))
+// document.write(text.toLowerCase(text))
+
+// document.write(text.toUpperCase(text))
+// document.write(text1.replaceAll(text1))  ==============undifened=======================
+// document.write(text1.charAt(text1))   ==================first leeter show
+// document.write(text1.charCodeAt(text1))====================char code 109================
+
 }
+
+
 
 
 
