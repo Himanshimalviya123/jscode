edf21f3dc6ce33ce2010f214a04d5a5cf46d4473
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-13 18:34:56 +0530

    update

---
+++ array.js
@@ -0,0 +1,12 @@
+function himanshi(){
+let aar1=["apple","mango","banana","grapes"]   
+// console.log(aar1.length);
+aar1.pop()
+// console.log(aar1);
+// aar1.push("papaya","orange")
+// console.log(aar1);
+// aar1.shift()
+// console.log(aar1);
+// aar1.unshift()
+console.log(aar1);
+}
