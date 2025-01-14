e9233affc096eca7c8a19d2dd9aa00d89da22070
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-14 18:33:52 +0530

    update

--- array.js
+++ array.js
@@ -1,12 +1,47 @@
 function himanshi(){
-let aar1=["apple","mango","banana","grapes"]   
+// let aar1=["apple","mango","banana","grapes"]   
 // console.log(aar1.length);
-aar1.pop()
+// aar1.pop()
 // console.log(aar1);
 // aar1.push("papaya","orange")
 // console.log(aar1);
 // aar1.shift()
 // console.log(aar1);
 // aar1.unshift()
-console.log(aar1);
+// console.log(aar1);
+//  =================================concat method=================================
+
+
+// let aar1=["apple","mango","banana","grapes"]   
+
+// let aar2=["onion","patato"]
+// let aar3=["1","2","3"]
+// console.log(aar1+aar2);
+// console.log(aar1.concat(aar2))  =============form of array===================
+
+// console.log(aar1.concat(aar2,aar3))
+// ==================================reverse method=============================
+
+
+//  let aar1=["apple","mango","banana","grapes"]   
+ 
+
+// console.log(aar1.reverse)
+
+
+// ===================slice method============================
+// let aar1=["apple","mango","banana","grapes"] 
+// let aar1=["1","2","3","5","7"]  
+// console.log(aar1.slice(2,5));
+// console.log(aar1.slice(0,-1));
+// =====================splice method=====================
+
+//  console.log(aar1.splice(2,0,8));
+//  console.log(aar1.splice(2,2,8));
+
+//  console.log(aar1);
+
+let aar1=[1,3,7,6,2,4,5]
+console.log(aar1.sort);
+
 }
