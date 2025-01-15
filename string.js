90ef398517552eead3aab3e71eb80e39be937587
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-16 00:27:43 +0530

    update

--- string.js
+++ string.js
@@ -28,7 +28,7 @@ function tanisha(){
 // =========================string method===============
 //  let text="ThisIsString";
 //  document.write(text.slice(5,10))  ===========       to check slice of string
-let text="cybrom";
+// let text="cybrom";
 // document.write(text.substring(2,5)) 
 // let text="JavaScriptIsEasy";
 // document.write(text.slice(4,13))
@@ -36,14 +36,15 @@ let text="cybrom";
 //  let text1="himanshi;"
 //  let text1="malviya";
 // document.write(text1.concat(text2))
-document.write(text.toLowerCase(text))
+// document.write(text.toLowerCase(text))
 
 // document.write(text.toUpperCase(text))
-// let text="i am learning Javascript and Javascript is fun javascript"
+let text="i am learning Javascript and Javascript is fun javascript"
 //   document.write(text.replace("Javascript","js"))===========replace the word=====================
-// document.write(text.replace(/Javascript/g,"js"))  global=>g
-// document.write(text.replace(/Javascript /gi,"js")) =================== case in sensitive=i=============
-
+// document.write(text.replaceAll("Javascript","js"))
+  document.write(text.replace(/Javascript/g,"js")) // global=>g
+//  document.write(text.replace(/Javascript /gi,"js")) //=================== case in sensitive=i=============
+// document.write(text1.charAt(5)) 
 //  document.write(text1.charAt(5))  ====================sky value return
 //   document.write(text1.indexOf("a"))==============to check the index no. of the char
 // let text="i am learning Javascript and Javascript is fun javascript"
