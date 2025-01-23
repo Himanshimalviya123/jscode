4dc24f59ee1ba872ce1a7cb1569c14c91dbd107d
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-23 17:31:58 +0530

    update

---
+++ dom1.js
@@ -0,0 +1,72 @@
+//function shiv(){
+   // document.getElementById("")
+   // document.getElementsByClassName("")
+  //  document.querySelector("")
+    //let para=document.querySelector("#new")
+    //para.innerHTML="HELLO I AM DEVELOPER"
+
+
+//}  
+function textchange(){
+    let change= document.querySelector("#box");
+    change.style.backgroundImage="url('img 1.jpg')";         //backgroung img
+ 
+   }
+   function new1(){
+    let change= document.querySelector("#box");
+    change.style.backgroundImage="none";
+   }
+    /*function textchange(){
+     let change= document.querySelector("#box")
+     change.innerHTML="<h1>Hello Cybrom</h1>"  
+     change.style.color="red"
+     change.style.backgroundColor="orange"
+     document.body.style.backgroundColor="gray"
+     
+    }
+    function shiv(){
+     let change= document.querySelector("#box")
+     change.innerHTML="<h1>Hello Cybrom</h1>"  
+     change.style.color="red"
+     change.style.backgroundColor="green" 
+     
+    }
+    function ram(){
+     document.body.style.backgroundColor="white"
+    }*/
+  /* function ram() {
+    let change= document.querySelector("#new")
+    if(change.style.display==='none'){
+       change.style.display='block';
+    }
+    else{
+       change.style.display='none';
+    }*/
+ 
+   /* let change1=0;
+    function ram() {
+       if(change1==1){
+          change.style.display="none";
+          change.style.display="block";
+          change =0;
+ 
+       }
+       else{
+          change.style.display ="none";
+          change1=1;
+       }*/
+ 
+    /*let change=  document.querySelector("#new")
+    let change1= document.querySelector("#btn1"))
+   change1.addEventListener('click',()=>{)
+       if(change.style.display==='none'){
+          change.style.display='block';
+       }
+       else{
+          change.style.display='none';
+       }
+ })*/
+    
+ 
+   // }
+ //  ram();
---
+++ object.js
@@ -0,0 +1,32 @@
+//destructuring//
+// let students={
+//     name:"sid",
+//     age:45,
+//     city:"bhopal",
+
+// }
+// // let{name,age,city}=students
+// console.log(n);
+
+// const student1={
+
+// ********************normal function***********************
+// ******************anonyms******************************
+// let go=function(){
+//     console.log("anonyms");
+    
+// }
+// go();
+// *********************IIFE FUNCTION*********************
+// (
+//     function(){
+//         console.log("iife");
+        
+//     }
+// )()
+// **********************arrow function(=>) /special type of anonymous function**************************
+let go=()=>{
+console.log("arrow");
+
+}
+go()
