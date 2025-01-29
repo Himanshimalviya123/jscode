c257edca595977fda78ee2e2afe67339753478fc
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-01-29 15:08:48 +0530

    update

--- aoo.js
+++ aoo.js
@@ -25,16 +25,23 @@
 // })
 // ===========================array of objrct======================================
 let aoo=[{ 
+    Sno:1,
+    class:4,
     name:"sid",
     age:25,
     city:"bopal",
+
 },
 {
+    Sno:2,
+    class:12,
     name:"himanshi",
     age:22,
     city:"pune",
 },
 {
+    Sno:3,
+    class:6,
     name:"shubham",
     age:25,
     city:"indore",
@@ -42,46 +49,54 @@ let aoo=[{
 
 },
 {
+    Sno:4,
+    class:11,
     name:"sakshi",
     age:15,
     city:"pune",
 },
 {
+    Sno:5,
+    class:7,
     name:"sonu",
     age:15,
     city:"pune",
 },
 {
+    Sno:6,
+    class:9,
     name:"pratik",
     age:15,
     city:"nagpur",
 },
 {
+    Sno:7,
+    class:10,
     name:"tushar",
     age:35,
     city:"hyderabad",
 },
 {
+    Sno:8,
+    class:8,
     name:"soumya",
     age:26,
     city:"betul",
 },
 {
+    Sno:9,
+    class:6,
     name:"khushal",
     age:45,
     city:"chennai",
 },
 {
+    Sno:10,
+    class:10,
     name:"era",
     age:36,
     city:"pune",
-},
-{
-    name:"likhit",
-    age:25,
-    city:"kolkata",
-}
-]
+}]
 //  ans=aoo.map((e)=>{
 //     return e.name+e.age+e.city
 // })
@@ -100,7 +115,9 @@ let ans=aoo.map((e)=>{
 show.innerHTML+=
 `
 <div>
-<h1 id="name">${e.name}</h1>
+<h1 id="name">${e.Sno}</h1>
+<h1>${e.class}</h1>
+<h1>${e.name}</h1>
 <h1>${e.age}</h1>
 <h1>${e.city}</h1>
 <div
