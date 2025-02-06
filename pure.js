213c305e153352bfd30c50f58f2ef42d627cd885
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-06 17:33:30 +0530

    update

--- pure.js
+++ pure.js
@@ -7,8 +7,12 @@
 // abd.addEventListener("click",()=>{
 // abc.style.backgroundColor=="red"
 // })
-let show=document.querySelector("#inp")
-let see=document.querySelector("#head")
-show.addEventListener("keydown",(e)=>{
-    see.innerHTML=(e.key)
-})
+// let show=document.querySelector("#inp")
+// let see=document.querySelector("#head")
+// show.addEventListener("keydown",(e)=>{
+//     see.innerHTML=(e.key)
+let show=document.querySelector("#head")
+ let see=document.querySelector("#inp")
+see.addEventListener("input",(e)=>{
+    show.innerHTML=(see.value)
+ })
