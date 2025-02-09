868779903e11a8febb96d907c367221904293bfc
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-09 22:37:12 +0530

    update

---
+++ bom.js
@@ -0,0 +1,107 @@
+// let abc=prompt("enter your age")
+// {
+//     if(abc>=18)
+//     {
+//         alert("eligible")
+//     }
+//     else
+//     {
+//            alert("not eligible") 
+//     }
+// }
+
+// let abc=prompt("you bank balance upto 1000 rs" )
+// {
+//     if(abc>=1000)
+//     {
+//         alert("welcome")
+//     }
+//     else
+//     {
+//         alert("out")
+//     }
+// }
+
+// let abc=confirm("if they complete a course and have a certificate ")
+// {
+//     if( abc==true)
+//     { 
+//         alert("yes")
+//     }
+//     else{
+//         alert("no")
+//     }
+
+// }
+// let abc=confirm("areyou sure")
+// {
+//     if(abc==true)
+//     {
+//         alert("item deleted")
+//     }
+//     else{
+//         alert("cancelled")
+//     }
+// }
+// let abc=alert("hello cybrom")
+// let abc=prompt("exam scrore")
+// {
+//     if(abc>33)
+//     {
+//         alert("pass")
+//     }
+//     else{
+//         alert("fail")
+//     }
+// }
+// ============================================================================
+// let abc=prompt("enter the no.")
+// {
+//     if(abc%=2)
+//     {
+//         alert("odd");
+
+//     }
+//     else{
+//         alert("even");
+//     }
+// }
+// ==============================================================
+// let abc=confirm("you are 10 passed out")
+// let pbc=confirm("you are 12 passed out")
+// if(abc && pbc)
+// {
+//     alert("yes")
+
+// }
+// else
+// {
+//     alert("no")
+// }
+// ===============================================================================
+// let abc=confirm("ask the user have completed your course")
+// let efg=confirm("do you have certificate?")
+//  if(abc && efg)
+//      {
+//       alert("completed");
+//  }
+//  else{
+//      alert("not completed"); 
+//   }
+// ********************************************************************************
+// var age=20;
+// var age=29;
+// console.log(age);
+// console.log(age);
+// var age=76;
+// ==================================================string==========
+let abc="cybrom is a technology institute"
+ console.log(abc);
+ 
+// document.write(abc.length)
+// document.write(abc.slice(3,9))
+let pvc="in bhopal"
+// document.write(abc.concat(pvc))
+// console.log(abc.concat(pvc));
+document .write
+
